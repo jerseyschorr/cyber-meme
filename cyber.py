cd825c37343d51b2f373667641f36e8a6a2cd628
Eric Schorr <eric.schorr@adp.com>
2019-03-12 13:31:31 -0400

    matrix works now

--- cyber.py
+++ cyber.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python
 import readchar
 import looptest
+import matrix
 
 def cyber():
-	while True:
+    while True:
 
-		# Read a key
-		key = readchar.readkey()
-		if(key == 'a'):
-			looptest.do_print()
-			print("pressed a...")
-		elif(key == 'z'):
-			print("pressed z...")
-		elif(key == '\r'):
-			print("Exiting...")
-			break
+        # Read a key
+        key = readchar.readkey()
+        if(key == 'a'):
+            looptest.do_print()
+            print("pressed a...")
+        elif(key == 'm'):
+            print("Starting Matrix")
+            matrix.neo()
+            print("Stopping Matrix")
+        elif(key == '\r'):
+            print("Exiting...")
+            break
 
 if __name__ == "__main__":
-	cyber()
+    cyber()
--- looptest.py
+++ looptest.py
@@ -1,9 +1,6 @@
 import threading
 import time
-from getch import *
-
-def testThread(num):
-    print(num)
+from getch import getch
 
 def input_thread(a_list):
     getch()
@@ -14,7 +11,7 @@ def do_print():
     threading.Thread(target=input_thread, args=(a_list,)).start()
     while not a_list:
       time.sleep(.1)
-      print("Hi Mom!")
+      print("Hi Mom!\n")
 
 if __name__ == "__main__":
     do_print()
