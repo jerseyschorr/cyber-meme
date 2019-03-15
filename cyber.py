6e0a4b3a37a4b319543ed4a7fc8283adba16988f
Eric Schorr <eric.schorr@adp.com>
2019-03-14 21:58:35 -0400

    Added Error and Heart

--- cyber.py
+++ cyber.py
@@ -2,6 +2,7 @@
 import readchar
 import looptest
 import matrix
+import showpng
 
 def cyber():
     while True:
@@ -9,12 +10,17 @@ def cyber():
         # Read a key
         key = readchar.readkey()
         if(key == 'a'):
-            looptest.do_print()
-            print("pressed a...")
-        elif(key == 'm'):
             print("Starting Matrix")
             matrix.neo()
             print("Stopping Matrix")
+        elif(key == 'q'):
+            print("Starting Error")
+            showpng.run('/home/pi/cyber-mime/error.png')
+            print("Stopping Error")
+        elif(key == 'z'):
+            print("Starting Heart")
+            showpng.run('/home/pi/cyber-mime/heart.png')
+            print("Stopping Heart")
         elif(key == '\r'):
             print("Exiting...")
             break
